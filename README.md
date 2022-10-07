27eadf15acc52dc1c728f7e18283e85eb2d50b8a
nothankyouwdym <105501075+nothankyouwdym@users.noreply.github.com>
2022-10-07 12:26:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-ivkhst
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-ivkhst)
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+
+class App extends Component{
+  render(){
+    return(
+      <section>
+        <h1>Test</h1>
+      </section>
+    )
+  }
+}
+
+
+export default App
